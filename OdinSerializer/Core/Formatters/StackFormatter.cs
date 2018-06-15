20de25f47c6512802f31f9de2625d33cab67415a
Tor Esa Vestergaard <tor_vestergaard@hotmail.com>
2018-06-15 09:22:20 +0200

    [Add] Added StackFormatter to support serializing Stack<T> properly.

---
+++ OdinSerializer/Core/Formatters/StackFormatter.cs
@@ -0,0 +1,153 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StackFormatter.cs" company="Sirenix IVS">
+// Copyright (c) 2018 Sirenix IVS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using OdinSerializer;
+
+[assembly: RegisterFormatter(typeof(StackFormatter<>))]
+
+namespace OdinSerializer
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Custom generic formatter for the generic type definition <see cref="Stack{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The element type of the formatted stack.</typeparam>
+    /// <seealso cref="BaseFormatter{System.Collections.Generic.Stack{T}}" />
+    public class StackFormatter<T> : BaseFormatter<Stack<T>>
+    {
+        private static readonly Serializer<T> TSerializer = Serializer.Get<T>();
+        private static object List_LOCK = new object();
+        private static List<T> List = new List<T>();
+
+        static StackFormatter()
+        {
+            // This exists solely to prevent IL2CPP code stripping from removing the generic type's instance constructor
+            // which it otherwise seems prone to do, regardless of what might be defined in any link.xml file.
+
+            new StackFormatter<int>();
+        }
+
+        public StackFormatter()
+        {
+        }
+
+        /// <summary>
+        /// Returns null.
+        /// </summary>
+        /// <returns>
+        /// A null value.
+        /// </returns>
+        protected override Stack<T> GetUninitializedObject()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Provides the actual implementation for deserializing a value of type <see cref="T" />.
+        /// </summary>
+        /// <param name="value">The uninitialized value to serialize into. This value will have been created earlier using <see cref="BaseFormatter{T}.GetUninitializedObject" />.</param>
+        /// <param name="reader">The reader to deserialize with.</param>
+        protected override void DeserializeImplementation(ref Stack<T> value, IDataReader reader)
+        {
+            string name;
+            var entry = reader.PeekEntry(out name);
+
+            if (entry == EntryType.StartOfArray)
+            {
+                try
+                {
+                    long length;
+                    reader.EnterArray(out length);
+                    value = new Stack<T>((int)length);
+
+                    // We must remember to register the stack reference ourselves, since we return null in GetUninitializedObject
+                    this.RegisterReferenceID(value, reader);
+
+                    // There aren't any OnDeserializing callbacks on stacks.
+                    // Hence we don't invoke this.InvokeOnDeserializingCallbacks(value, reader, context);
+                    for (int i = 0; i < length; i++)
+                    {
+                        if (reader.PeekEntry(out name) == EntryType.EndOfArray)
+                        {
+                            reader.Context.Config.DebugContext.LogError("Reached end of array after " + i + " elements, when " + length + " elements were expected.");
+                            break;
+                        }
+
+                        value.Push(TSerializer.ReadValue(reader));
+
+                        if (reader.IsInArrayNode == false)
+                        {
+                            // Something has gone wrong
+                            reader.Context.Config.DebugContext.LogError("Reading array went wrong at position " + reader.Stream.Position + ".");
+                            break;
+                        }
+                    }
+                }
+                finally
+                {
+                    reader.ExitArray();
+                }
+            }
+            else
+            {
+                reader.SkipEntry();
+            }
+        }
+
+        /// <summary>
+        /// Provides the actual implementation for serializing a value of type <see cref="T" />.
+        /// </summary>
+        /// <param name="value">The value to serialize.</param>
+        /// <param name="writer">The writer to serialize with.</param>
+        protected override void SerializeImplementation(ref Stack<T> value, IDataWriter writer)
+        {
+            try
+            {
+                writer.BeginArrayNode(value.Count);
+
+                lock (List_LOCK)
+                {
+                    List.Clear();
+
+                    foreach (var element in value)
+                    {
+                        List.Add(element);
+                    }
+
+                    for (int i = List.Count - 1; i >= 0; i--)
+                    {
+                        try
+                        {
+                            TSerializer.WriteValue(List[i], writer);
+                        }
+                        catch (Exception ex)
+                        {
+                            writer.Context.Config.DebugContext.LogException(ex);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                writer.EndArrayNode();
+            }
+        }
+    }
+}
