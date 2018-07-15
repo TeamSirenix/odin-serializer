c4d51eaa1d856eaa9e32feb08b28c70fde71f75d
Tor Esa Vestergaard <tor_vestergaard@hotmail.com>
2018-07-15 16:44:19 +0200

    [Fix] Made AssemblyUtilities.IsDynamic more robust.

--- OdinSerializer/Utilities/Misc/AssemblyUtilities.cs
+++ OdinSerializer/Utilities/Misc/AssemblyUtilities.cs
@@ -419,7 +419,8 @@ namespace OdinSerializer.Utilities
         public static bool IsDynamic(this Assembly assembly)
         {
             if (assembly == null) throw new ArgumentNullException("assembly");
-            return assembly.GetType().Name == "AssemblyBuilder";
+            // Will cover both System.Reflection.Emit.AssemblyBuilder and System.Reflection.Emit.InternalAssemblyBuilder
+            return assembly.GetType().FullName.EndsWith("AssemblyBuilder") || assembly.Location == null || assembly.Location == "";
         }
 
         /// <summary>
