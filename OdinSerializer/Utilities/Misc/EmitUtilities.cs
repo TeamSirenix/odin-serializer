cb71190c1957f6ecc54f999323a7f40325b8420f
DESKTOP-HNVPF6P\Mikkel <mikkel@sirenix.net>
2018-11-23 16:10:04 +0100

    Added error to the source code for when Odin is compiled as source code in Unity with .NET Standard 2.0 API compatibility level.

--- OdinSerializer/Utilities/Misc/EmitUtilities.cs
+++ OdinSerializer/Utilities/Misc/EmitUtilities.cs
@@ -16,6 +16,10 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+#if NET_STANDARD_2_0
+#error Odin Inspector is incapable of compiling source code against the .NET Standard 2.0 API surface. You can change the API Compatibility Level in the Player settings.
+#endif
+
 #if (UNITY_EDITOR || UNITY_STANDALONE) && !ENABLE_IL2CPP
 #define CAN_EMIT
 #endif
